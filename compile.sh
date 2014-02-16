3b274f93e50f912c87d4ec439c1a378d5af74034
Sergio Campamá <sergiocampama@gmail.com>
2014-02-16 02:01:48 -0300

    Removing old file

--- compile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-gcc -c -g src/hash.c -Iheaders -o bin/hash.o
-gcc -c -g src/list.c -Iheaders -o bin/list.o
-gcc -c -g src/main.c -Iheaders -o bin/main.o
-
-gcc bin/hash.o bin/list.o bin/main.o -o bin/main
